9f58752fe2467ab4ee981f98a98b61d47fe78ef4
Tomas <tomasmaggio@gmail.com>
2016-11-17 22:41:24 +1300

    Update JoinAD.sh

--- JoinAD.sh
+++ JoinAD.sh
@@ -49,7 +49,7 @@ if [[ ! -z $APT_GET_CMD ]]; then
 	# Change homedir format
 	sed -i s:'fallback_homedir = /home/%u@%d':'fallback_homedir = /home/%d/%u':g /etc/sssd/sssd.conf
 	# Set default domain to avoid using it on login prompt
-	sed -i '/sssd/ a default_domain_suffix=$DOMAIN' /etc/sssd/sssd.conf
+	sed -i "/sssd/ a default_domain_suffix=$DOMAIN" /etc/sssd/sssd.conf
 	# Change FQDN for usernames
 	sed -i s:'use_fully_qualified_names = True':'use_fully_qualified_names = False':g /etc/sssd/sssd.conf
 	# Restart sssd
@@ -74,7 +74,7 @@ if [[ ! -z $YUM_CMD ]]; then
 	# Change homedir format
 	sed -i s:'fallback_homedir = /home/%u@%d':'fallback_homedir = /home/%d/%u':g /etc/sssd/sssd.conf
 	# Set default domain to avoid using it on login prompt
-	sed -i '/sssd/ a default_domain_suffix=$DOMAIN' /etc/sssd/sssd.conf
+	sed -i "/sssd/ a default_domain_suffix=$DOMAIN" /etc/sssd/sssd.conf
 	# Change FQDN for usernames
 	sed -i s:'use_fully_qualified_names = True':'use_fully_qualified_names = False':g /etc/sssd/sssd.conf
 	# Restart sssd
