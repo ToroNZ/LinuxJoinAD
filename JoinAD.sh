f8b79a8a4f5584843357b091475f5299d06fd59f
Tomas <tomasmaggio@gmail.com>
2016-11-20 21:22:58 +1300

    Update JoinAD.sh

--- JoinAD.sh
+++ JoinAD.sh
@@ -21,11 +21,11 @@ elif [[ ! -z $APT_GET_CMD ]]; then
 	read KEY
 	mkdir -p /var/lib/samba/private
 	if ! $(sudo which realmd 2>/dev/null); then
-	sudo DEBIAN_FRONTEND=noninteractive apt-get -y install krb5-user
-	aptitude install samba sssd
+		sudo DEBIAN_FRONTEND=noninteractive apt-get -y install krb5-user
+		aptitude install samba sssd
 	fi
  	if ! $(sudo which ntpd 2>/dev/null); then
-	aptitude install ntp
+		aptitude install ntp
 	fi
 else
 	echo "error can't install package $PACKAGE"
